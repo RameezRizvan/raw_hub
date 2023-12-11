583b0a176bf40852e988cfd508efa6b5bf570a55
RameezRizvan <rameezrizvan04@gmail.com>
2023-12-11 16:10:57 +0530

    feat: Added payment doctype, linked to students doctype and created print format for student payment

--- rawscholar_app/rawscholar_app/doctype/student/student.py
+++ rawscholar_app/rawscholar_app/doctype/student/student.py
@@ -6,4 +6,23 @@ from frappe.model.document import Document
 
 
 class Student(Document):
-	pass
+
+    def validate(self):
+        self.validate_payments()
+
+    def validate_payments(self):
+        # Validate the payments as needed
+        pass
+
+    def on_update(self):
+        # Additional logic on update, if necessary
+        pass
+
+    @staticmethod
+    def get_query(doctype, txt, searchfield, start, page_len, filters):
+    # Customize the query to filter payments based on the selected student
+        return f"""
+          SELECT name1, amount
+          FROM `Payment`
+          WHERE Student = '{filters.get('Student')}'
+        """
